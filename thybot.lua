b1e6ce9b3d141f13077131616a6f25d65947ad67
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-05 16:36:13 -0600

    Update thybot.lua

--- thybot.lua
+++ thybot.lua
@@ -1,5 +1,17 @@
 local discordia = require "discordia"
+local client = discordia.client
+--enables discordia extensions
+discordia.extensions()
+--definitions
+local enums = discordia.enums
+local class = discordia.class
+local fmt = string.format
+--extras
+local http = require('coro-http')
+local json = require('json')
+local mutex = discordia.Mutex()
 
-mods = nil
+mods = require "data/mods"
 
 --to be finished
+
