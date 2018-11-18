73680764b7d7dd172e1d3bc4a2d82ec01894ad77
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-18 10:44:17 -0600

    Update demo.lua

--- data/modules/demo.lua
+++ data/modules/demo.lua
@@ -3,6 +3,7 @@ local mod = {}
 function mod.init()
 end
 function mod.messageCreate(message)
+  if message.content == "th-test" then message.reply("demo module executed.\n\n~~execute order 66~~") end
 end
 
 return mod
