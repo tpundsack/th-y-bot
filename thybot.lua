a9ea0acc4b0264d250bbbbbfce8f8093a71f5cf9
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-18 10:28:50 -0600

    Update thybot.lua

--- thybot.lua
+++ thybot.lua
@@ -22,6 +22,25 @@ client:on("messageCreate", function(message)
     for i, k in pairs(mods) do
         if k.messageCreate then k.messageCreate(message) end
       end
+        if message.content == "reload" then
+            msg = ""
+			os.execute('git pull "https://github.com/tpundsack/th-y-bot" --allow-unrelated-histories')
+		for i, k in pairs(package.loaded) do
+			if string.match(i, "Data/mods/") then
+			local a = string.match(i, "Data/mods/(.*)")
+			package.loaded[i] = nil
+			configs[a] = require(i)
+			msg = msg .."reloaded `".. i.."`\n"
+		end
+		if i == "wiki" then
+		package.loaded[i] = nil
+		configs["wiki"] = require(i)
+		msg = msg .."reloaded `".. i.."`\n"
+	end
+		end
+		message.author:send(msg) message.author:getPrivateChannel():close()
+		message:delete()
+            end
     end)
 
 client:run(token)
